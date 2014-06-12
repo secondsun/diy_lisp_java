905de4b9bd24324835d2808727daf847b93a0346
Summers Pittman <secondsun@gmail.com>
2014-06-12 11:14:25 -0400

    Adding in more tests

--- src/test/java/net/saga/diy/lisp/parser/CompilerTest.java
+++ src/test/java/net/saga/diy/lisp/parser/CompilerTest.java
@@ -20,8 +20,11 @@ package net.saga.diy.lisp.parser;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import static net.saga.diy.lisp.parser.EvaluatorTest.run;
 import static net.saga.diy.lisp.parser.Parser.parse;
+import net.saga.diy.lisp.parser.types.LispException;
 import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
@@ -47,7 +50,7 @@ public class CompilerTest {
     @Test
     /**
      * The compiler should generate a main method which can be called. This
-     * method will return a list or an atom representing the evaluated
+     * method will return a list or an atom representing the compiled
      * expressions.
      */
     public void testCompilerGeneratesMainMethod() throws NoSuchMethodException {
@@ -67,11 +70,93 @@ public class CompilerTest {
         assertFalse((boolean) run("#f"));
     }
 
-    public static Object run(String program) throws NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    /* ...and so should integers. */
+    @Test
+    public void testCompileInteger() {
+        assertEquals(42, (int) run("42"));
+    }
+
+    /*
+     * When a call is done to the `quote` form, the argument should be returned without
+     * being compiled.
+     * 
+     * (quote foo) -> foo
+     */
+    @Test
+    public void compileQuote() {
+        assertEquals("foo", run("( quote foo)"));
+        Assert.assertArrayEquals(new Object[] { 1, 2, false }, (Object[]) run("(quote (1 2 #f))"));
+    }
+
+    /*
+     * The `atom` form is used to determine whether an expression is an atom.
+     * 
+     * Atoms are expressions that are not list, i.e. integers, booleans or symbols.
+     * Remember that the argument to `atom` must be compiled before the check is done.
+     */
+    @Test
+    public void compileAtom() {
+        assertTrue((boolean) run("(atom #t)"));
+        assertTrue((boolean) run("(atom #f)"));
+        assertTrue((boolean) run("(atom 42)"));
+        assertTrue((boolean) run("(atom 'foo)"));
+        assertFalse((boolean) run("(atom '(1 2))"));
+    }
+
+    /* The `eq` form is used to check whether two expressions are the same atom. */
+    @Test
+    public void compileEq() {
+        assertTrue((boolean) run("(eq 1 1)"));
+        assertFalse((boolean) run("(eq 1 2)"));
+        assertTrue((boolean) run("(eq 'foo 'foo)"));
+        assertFalse((boolean) run("(eq 'foo 'bar)"));
+        assertFalse((boolean) run("(eq '(1 2 3) '(1 2 3))"));
+    }
+
+    /*
+     * To be able to do anything useful, we need some basic math operators.
+     * 
+     * Since we only operate with integers, `/` must represent integer division.
+     * `mod` is the modulo operator.
+     */
+    @Test
+    public void compileMath() {
+        assertEquals(4, run("(+ 2 2)"));
+        assertEquals(1, run("(- 2 1)"));
+        assertEquals(3, run("(/ 6 2)"));
+        assertEquals(3, run("(/ 7 2)"));
+        assertEquals(6, run("(* 2 3)"));
+        assertEquals(1, run("(mod 7 2)"));
+        assertEquals(true, run("(> 7 2)"));
+        assertEquals(false, run("(< 7 2)"));
+        assertEquals(false, run("(> 7 7)"));
+    }
+
+    @Test
+    public void compileNestedMath() {
+        assertEquals(8, run("(+ 2 (+ 3 3))"));
+        assertEquals(1, run("(- (+ 0 2) 1)"));
+        assertEquals(30, run("(* 10 (/ 6 2))"));
+
+    }
+
+    /* The math functions should only allow numbers as arguments. */
+    @Test(expected = LispException.class)
+    public void mathOnlyUsesInteger() {
+        assertEquals(false, run("> 7 'foo"));
+    }
+
+    public static Object run(String program){
+        try {
         Class<?> klass = Compiler.compile(parse(program));
         Object instance = klass.newInstance();
         Method method = klass.getMethod("main", (Class[])null);
-        return method.invoke(instance, (Class[])null);
+        return method.invoke(instance, (Object[])null);
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | SecurityException | IllegalArgumentException | InvocationTargetException e) {
+            System.err.println(e);
+            e.printStackTrace(System.err);
+            throw new RuntimeException(e);
+        }
     }
 
 }
